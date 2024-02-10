17372075cd540374f8b40d40b9f37b459c39e5d6
rushil <85025732+rushilhacker1@users.noreply.github.com>
2024-02-10 11:25:15 +0000

    ok

--- index.js
+++ index.js
@@ -1,108 +1,79 @@
 //imports
-import { Database } from "bun:sqlite";
+const { MongoClient } = require("mongodb");
 
 //db init
-const db = new Database("mydb.sqlite", { create: true });
-db.exec("PRAGMA journal_mode = WAL;");
-db.run(`CREATE TABLE IF NOT EXISTS Customer (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    image_url VARCHAR(1000),
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(1100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    rating INTEGER NOT NULL,
-    balance INTEGER NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    destination VARCHAR(100) NOT NULL
-)`)
-db.run(`
-CREATE TABLE IF NOT EXISTS Driver (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    image_url VARCHAR(1000),
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(1100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    rating INTEGER NOT NULL,
-    status VARCHAR(100) NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    destination VARCHAR(100) NOT NULL
-)
-`)
+const uri =
+  "mongodb+srv://rushil:ankurrushil1@cluster0.uizpbcq.mongodb.net/?retryWrites=true&w=majority";
+// db.run(`
+// CREATE TABLE IF NOT EXISTS Driver (
+// id INTEGER PRIMARY KEY AUTOINCREMENT,
+// image_url VARCHAR(1000),
+// name VARCHAR(100) NOT NULL,
+// email VARCHAR(1100) NOT NULL UNIQUE,
+// password VARCHAR(100) NOT NULL,
+// rating INTEGER NOT NULL,
+// status VARCHAR(100) NOT NULL,
+// location VARCHAR(100) NOT NULL,
+// destination VARCHAR(100) NOT NULL
+// )
+// `)
 
 //global variables
-const port = 3000 | process.env.PORT
+const port = 80 | process.env.PORT;
+const client = new MongoClient(uri);
 
 //fnctions
 function log(log) {
-  console.log(log)
+  console.log(log);
 }
-function hash(pass) {
+function hasher(pass) {
   const hasher = new Bun.CryptoHasher("sha256");
   hasher.update(pass);
   const retu = hasher.digest("base64");
-  return retu
+  return retu;
 }
 
-console.log("Hello via Bun!");
 const server = Bun.serve({
   development: true,
-  port: 80,
+  port: port,
+
   async fetch(req) {
     const url = new URL(req.url);
     const urlParams = new URLSearchParams(url.search);
+
     switch (url.pathname) {
       case "/customers":
         switch (req.method) {
           case "GET":
             const need = urlParams.get("id");
             if (need == null) {
-              const query = db.query("SELECT * FROM Customer")
-              log("q")
-              return new Response(JSON.stringify(query.values()))
+              const query = db.query("SELECT * FROM Customer");
+              return new Response(JSON.stringify(query.values()));
             } else {
-            const query = db.query("SELECT * FROM Customer where id = ".concat(need))
-            const result = query.get()
-            log(result)
-            log(need)
-            if (result == null) {
-              throw new Error("undefined");
-            } else {
-              return new Response(JSON.stringify(result));
+              const query = db.query(
+                "SELECT * FROM Customer where id = ".concat(need)
+              );
+              const result = query.get();
+              if (result == null) {
+                throw new Error("undefined");
+              } else {
+                return new Response(JSON.stringify(result));
+              }
             }
-          }
+
           case "PUT":
-            log("sjdrw")
             const data = await req.json();
-            const sql = `
-            INSERT INTO Customer (
-                image_url, 
-                name, 
-                email, 
-                password, 
-                rating, 
-                balance, 
-                location, 
-                destination
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        `;
-        
-        db.run(sql, [
-            data.image_url, 
-            data.name, 
-            data.email, 
-            data.password, 
-            data.rating, 
-            data.balance, 
-            data.location, 
-            data.destination
-        ]);
+          case "/":
+            return new Response(Bun.file("./src/html/home.htm"));
+          case "/contact":
+            return new Response(Bun.file("./src/html/contact_us.htm"));
+          case "/favicon.ico":
+            return new Response(Bun.file("./public/favicon.ico"));
+          default:
+            return new Response(Bun.file("./src/html/404.htm"));
         }
-      case "/":
-        return new Response(Bun.file("./public/home.htm"))
-      default:
-        return new Response(Bun.file("./public/404.htm"))
     }
   },
 });
-log("app running on port ".concat(port))
-log(hash("w"))
+
+log(`app running on port ${port} `);
