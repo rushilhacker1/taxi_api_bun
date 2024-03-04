8adb3ded42cb95dfd96ee9d5f9cd853227fde5e6
rushil <85025732+rushilhacker1@users.noreply.github.com>
2024-03-04 05:30:49 +0000

    cast on odespace

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-//imports
-const { MongoClient } = require("mongodb");
-
-//db init
-const uri =
-  "mongodb+srv://rushil:ankurrushil1@cluster0.uizpbcq.mongodb.net/?retryWrites=true&w=majority";
-// db.run(`
-// CREATE TABLE IF NOT EXISTS Driver (
-// id INTEGER PRIMARY KEY AUTOINCREMENT,
-// image_url VARCHAR(1000),
-// name VARCHAR(100) NOT NULL,
-// email VARCHAR(1100) NOT NULL UNIQUE,
-// password VARCHAR(100) NOT NULL,
-// rating INTEGER NOT NULL,
-// status VARCHAR(100) NOT NULL,
-// location VARCHAR(100) NOT NULL,
-// destination VARCHAR(100) NOT NULL
-// )
-// `)
 
 //global variables
-const port = 80 | process.env.PORT;
-const client = new MongoClient(uri);
+const port = process.env.PORT;
 
 //fnctions
 function log(log) {
@@ -42,27 +22,6 @@ const server = Bun.serve({
     const urlParams = new URLSearchParams(url.search);
 
     switch (url.pathname) {
-      case "/customers":
-        switch (req.method) {
-          case "GET":
-            const need = urlParams.get("id");
-            if (need == null) {
-              const query = db.query("SELECT * FROM Customer");
-              return new Response(JSON.stringify(query.values()));
-            } else {
-              const query = db.query(
-                "SELECT * FROM Customer where id = ".concat(need)
-              );
-              const result = query.get();
-              if (result == null) {
-                throw new Error("undefined");
-              } else {
-                return new Response(JSON.stringify(result));
-              }
-            }
-
-          case "PUT":
-            const data = await req.json();
           case "/":
             return new Response(Bun.file("./src/html/home.htm"));
           case "/contact":
@@ -74,6 +33,6 @@ const server = Bun.serve({
         }
     }
   },
-});
+);
 
 log(`app running on port ${port} `);
