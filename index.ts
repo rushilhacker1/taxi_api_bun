dba00a58ac25828aa3a181617c62ff01b1fbd016
rushil <rushiling121@hotmail.com>
2024-03-04 11:38:34 +0530

    dwe

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+
+const port = process.env.PORT || 80;
+
+
+function log(log:string) {
+    console.log(log);
+  }
+  function hasher(pass:string) {
+    const hasher = new Bun.CryptoHasher("sha256");
+    hasher.update(pass);
+    const retu = hasher.digest("base64");
+    return retu;
+  }
 
 const app = new Hono()
 
-const server = bun.server({
-    port:80,
-    fetch: app.fetch()
+app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }))
+app.use('/', serveStatic({path: './src/html/home.htm'}))
+app.use('/contect', serveStatic({path: './src/html/'}))
+app.get('*', serveStatic({ path: './src/html/404.htm' }))
+
+
+const server = Bun.serve({
+    port:port,
+    development: true,
+    fetch: app.fetch
 })
+
+log(`app running on port ${port} `);
